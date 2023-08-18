ba74d7d8d1faae7164a219114d8b34473b6da3b1
GabGou1 <gabrielriberikoa@gmail.com>
2023-08-18 10:48:56 -0300

    Configuração do controle de rota home

---
+++ goserviceapp/src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.soulcode.goserviceapp.controller;
+// Imprimir o html de acordo com o request
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+    @GetMapping
+    public String home(){
+        return "home";
+    }
+}
